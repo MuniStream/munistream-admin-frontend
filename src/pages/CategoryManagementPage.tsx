447974d89ff1636aae95b8d88296dd7a5d7bf58d
Paola Villarreal <paw@paw.mx>
2025-11-11 02:08:05 -0600

    Update package dependencies
    Update npm package-lock.json with latest dependency versions
    for admin dashboard compatibility

--- src/pages/CategoryManagementPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CategoryManagement from '../components/CategoryManagement';
-
-const CategoryManagementPage: React.FC = () => {
-  return <CategoryManagement />;
-};
-
-export default CategoryManagementPage;
--- src/pages/TeamManagementPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TeamManagement from '../components/TeamManagement';
-
-function TeamManagementPage() {
-  return <TeamManagement />;
-}
-
-export default TeamManagementPage;
